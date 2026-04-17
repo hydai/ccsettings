fc73fbbae4087ad2b67ac455d56237932e54d691
hydai <z54981220@gmail.com>
2026-04-17 20:40:32 +0800

    feat(updater): register tauri-plugin-updater in builder chain

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -20,6 +20,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .manage(state)
         .invoke_handler(tauri::generate_handler![
             commands::list_workspaces,
